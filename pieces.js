f43decaf4f7372ec4305c7a6c5904ae8bea01197
Aurelie Moustardier <naoorelie@gmail.com>
2024-02-29 20:23:38 +0100

    exercice map

--- pieces.js
+++ pieces.js
@@ -45,7 +45,7 @@ for (let i = 0; i < pieces.length; i++) {
 
 const boutonTrier = document.querySelector(".btn-trier")
 const boutonFilter = document.querySelector(".btn-filtrer")
-const boutonDecrisption = document.querySelector(".btn-btn-description")
+const boutonDescription = document.querySelector(".btn-description")
 const boutonDecroissant = document.querySelector(".btn-decroissant")
 
 boutonTrier.addEventListener("click", function () {
@@ -65,16 +65,69 @@ boutonTrier.addEventListener("click", function () {
 })
 
 boutonFilter.addEventListener("click", function () {
-    const piecesFiltrees = pieces.filter(function (pieces){
-        return pieces.prix <= 35;
+    const piecesFiltrees = pieces.filter(function (piece){
+        return piece.prix <= 35;
     })
-    console.log(piecesFiltrees)
+    console.log(piecesFiltrees);
 })
 
-boutonDecrisption.addEventListener("click", function (){
-    const piecesDescription = pieces.filter(function (pieces){
-        return pieces.description
+boutonDescription.addEventListener("click", function () {
+    const piecesFiltrees = pieces.filter(function (piece){
+        return piece.description;
     })
-    console.log(piecesDescription)
+    console.log(piecesFiltrees);
 })
+
+//afficher que les noms
+const noms = pieces.map(piece => piece.nom);
+for(let i = pieces.length -1 ; i >= 0; i--){
+    if(pieces[i].prix > 35){
+        noms.splice(i,1);
+    }
+}
+
+//Création de la liste
+const abordablesElements = document.createElement('ul');
+//Ajout de chaque nom à la liste
+for(let i=0; i < noms.length ; i++){
+    const nomElement = document.createElement('li');
+    nomElement.innerText = noms[i];
+    abordablesElements.appendChild(nomElement)
+}
+// Ajout de l'en-tête puis de la liste au bloc résultats filtres
+document.querySelector('.abordables')
+    .appendChild(abordablesElements);
+
+
+//récupérer la liste des pièces disponibles
+const titrePiece = pieces.map(piece => piece.nom)
+const Prix = pieces.map(piece => piece.prix)
+const descriptions = pieces.map(piece => piece.description)
+const enStock = pieces.map(piece => piece.disponibilite)
+console.log(enStock)
+//supprimer les pièces non disponibles
+for(let i = pieces.length -1 ; i >= 0; i--){
+    if(pieces[i].disponibilite === false){
+        enStock.splice(i,1);
+    }
+    console.log(enStock)
+}
+// EnStock correspond aux articles en stock
+//il faut faire maintenant apparaître le nom, la description et le prix
+
+//creation de l'élément :
+const PiecesDisponibles = document.createElement('ul');
+
+//boucle qui va permettre d'afficher le titre, la description et le prix :
+for(let i=0; i < enStock.length ; i++){
+    let disponibleElement = document.createElement('li');
+    disponibleElement.innerText = `${titrePiece[i]} -  ${Prix[i]} € : ${descriptions[i]}`;
+    PiecesDisponibles.appendChild(disponibleElement);
+}
+
+document.querySelector('.disponibles')
+    .appendChild(PiecesDisponibles);
+// nomElement + prixElement +  descriptionElement
+// si pieces[i] est en stock, alors affiche le nom, la description et le prix
+//const prixElement = document.createElement("p");
 
