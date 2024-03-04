2600a742b49752badb310885c6437c49fe40b169
Aurelie Moustardier <naoorelie@gmail.com>
2024-03-04 18:11:37 +0100

    exos api

--- pieces.js
+++ pieces.js
@@ -1,81 +1,99 @@
+import { ajoutListenersAvis, ajoutListenerEnvoyerAvis } from "./avis.js";
 // Récupération des pièces depuis le fichier JSON
-const reponse = await fetch("pieces-autos.json");
+const reponse = await fetch("http://localhost:8081/pieces/");
 const pieces = await reponse.json();
 
+ajoutListenerEnvoyerAvis()
 
+function genererPieces(pieces){
+    for (let i = 0; i < pieces.length; i++) {
 
-for (let i = 0; i < pieces.length; i++) {
-
-    const sectionFiches = document.querySelector(".fiches");
-
-    const pieceElement = document.createElement("article");
-
-    const imageElement = document.createElement("img");
-    imageElement.src = pieces[i].image;
-
-    const nomElement = document.createElement("h2");
-    nomElement.innerText = pieces[i].nom;
-
-    const prixElement = document.createElement("p");
-    prixElement.innerText = `Prix: ${pieces[i].prix} € (${pieces[i].prix < 35 ? "€" : "€€€"})`;
-
-    const categorieElement = document.createElement("p");
-    categorieElement.innerText = pieces[i].categorie ?? "(aucune catégorie)";
-
-    const descriptionElement = document.createElement("p");
-    descriptionElement.innerText = pieces[i].description ?? "aucune description";
-
-    const disponibiliteElement = document.createElement("p")
-    if (pieces[i].disponibilite === true) {
-        disponibiliteElement.innerText = "En stock"
-    } else {
-        disponibiliteElement.innerText = "Rupture de stock"
-    }
-
-
-    // Génération des balises
-    sectionFiches.appendChild(pieceElement);
-    pieceElement.appendChild(imageElement);
-    pieceElement.appendChild(nomElement);
-    pieceElement.appendChild(prixElement);
-    pieceElement.appendChild(categorieElement);
-    pieceElement.appendChild(descriptionElement);
-    pieceElement.appendChild(disponibiliteElement)
+        const article = pieces[i];
+        // Récupération de l'élément du DOM qui accueillera les fiches
+        const sectionFiches = document.querySelector(".fiches");
+        // Création d’une balise dédiée à une pièce automobile
+        const pieceElement = document.createElement("article");
+        // Création des balises 
+        const imageElement = document.createElement("img");
+        imageElement.src = article.image;
+        const nomElement = document.createElement("h2");
+        nomElement.innerText = article.nom;
+        const prixElement = document.createElement("p");
+        prixElement.innerText = `Prix: ${article.prix} € (${article.prix < 35 ? "€" : "€€€"})`;
+        const categorieElement = document.createElement("p");
+        categorieElement.innerText = article.categorie ?? "(aucune catégorie)";
+        const descriptionElement = document.createElement("p");
+        descriptionElement.innerText = article.description ?? "Pas de description pour le moment.";
+        const stockElement = document.createElement("p");
+        stockElement.innerText = article.disponibilite ? "En stock" : "Rupture de stock";
+        //Code ajouté
+        const avisBouton = document.createElement("button");
+        avisBouton.dataset.id = article.id;
+        avisBouton.textContent = "Afficher les avis";
+        
+        
+        // On rattache la balise article a la section Fiches
+        sectionFiches.appendChild(pieceElement);
+        pieceElement.appendChild(imageElement);
+        pieceElement.appendChild(nomElement);
+        pieceElement.appendChild(prixElement);
+        pieceElement.appendChild(categorieElement);
+        pieceElement.appendChild(descriptionElement);
+        pieceElement.appendChild(stockElement);
+        //Code aJouté
+        pieceElement.appendChild(avisBouton);
+    
+     }
+     ajoutListenersAvis();
 }
 
+//Premier affichage page
+genererPieces(pieces);
+
+// trier prix croissant
 const boutonTrier = document.querySelector(".btn-trier")
-const boutonFilter = document.querySelector(".btn-filtrer")
-const boutonDescription = document.querySelector(".btn-description")
-const boutonDecroissant = document.querySelector(".btn-decroissant")
 
 boutonTrier.addEventListener("click", function () {
     const piecesOrdonnees = Array.from(pieces);
     piecesOrdonnees.sort(function (a, b) {
         return a.prix - b.prix;
      });
-     console.log(piecesOrdonnees);
+     document.querySelector(".fiches").innerHTML = "";
+     genererPieces(piecesOrdonnees)
  });
 
+ // prix abordable
+ const boutonFilter = document.querySelector(".btn-filtrer")
+ boutonFilter.addEventListener("click", function () {
+    const piecesFiltrees = pieces.filter(function (piece){
+        return piece.prix <= 35;
+    })
+    document.querySelector(".fiches").innerHTML = "";
+     genererPieces(piecesFiltrees)
+})
+
+
+const boutonDescription = document.querySelector(".btn-description")
+const boutonDecroissant = document.querySelector(".btn-decroissant")
+
+// decroissant
  boutonDecroissant.addEventListener("click", function () {
     const piecesDecroissantes = Array.from(pieces);
     piecesDecroissantes.sort(function (a, b) {
         return b.prix - a.prix;
     });
-    console.log(piecesDecroissantes);
+    document.querySelector(".fiches").innerHTML = "";
+    genererPieces(piecesDecroissantes)
 })
 
-boutonFilter.addEventListener("click", function () {
-    const piecesFiltrees = pieces.filter(function (piece){
-        return piece.prix <= 35;
-    })
-    console.log(piecesFiltrees);
-})
 
+//afficher description
 boutonDescription.addEventListener("click", function () {
     const piecesFiltrees = pieces.filter(function (piece){
         return piece.description;
     })
-    console.log(piecesFiltrees);
+    document.querySelector(".fiches").innerHTML = "";
+     genererPieces(piecesFiltrees);
 })
 
 //afficher que les noms
@@ -112,8 +130,6 @@ for(let i = pieces.length -1 ; i >= 0; i--){
     }
     console.log(enStock)
 }
-// EnStock correspond aux articles en stock
-//il faut faire maintenant apparaître le nom, la description et le prix
 
 //creation de l'élément :
 const PiecesDisponibles = document.createElement('ul');
@@ -127,7 +143,15 @@ for(let i=0; i < enStock.length ; i++){
 
 document.querySelector('.disponibles')
     .appendChild(PiecesDisponibles);
-// nomElement + prixElement +  descriptionElement
-// si pieces[i] est en stock, alors affiche le nom, la description et le prix
-//const prixElement = document.createElement("p");
+
+    
+//Exos trier par prix
+const inputPrixMax = document.querySelector('#prix-max')
+inputPrixMax.addEventListener('input', function(){
+    const piecesFiltrees = pieces.filter(function(piece){
+        return piece.prix <= inputPrixMax.value;
+    })
+    document.querySelector(".fiches").innerHTML = "";
+    genererPieces(piecesFiltrees)
+})
 
